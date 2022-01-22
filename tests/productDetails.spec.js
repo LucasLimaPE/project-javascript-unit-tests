1d7f1aa65ee7ab4cb93fc7c11fc1f9e17e4cb228
LucasLimaPE <robertolucas011@gmail.com>
2022-01-22 11:14:14 -0300

    Adiciona requisito 6.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -27,8 +27,30 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+  it('Verifica se a função `productDetails é uma função', () => {
+    expect(typeof productDetails).toEqual('function');
+  });
+  it('Verifica se a função `productDetails` retorna um array', () => {
+    expect(Array.isArray(productDetails('mango', 'avocado'))).toEqual(true);
+  });
+  it('Verifica se o retorno da chamada da função `productDetails` contém dois itens dentro.', () => {
+    expect(productDetails('mango', 'avocado').length).toEqual(2);
+  });
+  it('Verifica se os dois itens dentro do array retornado pela função são objetos.', () => {
+    expect(typeof productDetails('mango', 'avocado')[0]).toEqual('object');
+    expect(typeof productDetails('mango', 'avocado')[1]).toEqual('object');
+  });
+  it('Verifica se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    expect(productDetails('mango', 'avocado')[0] !== productDetails('mango', 'avocado')[1])
+    .toEqual(true);
+  });
+  it('Verifica se os dois productIds terminam com 123.', () => {
+    expect(productDetails('mango', 'avocado')[0].details.productId === 'mango123').toEqual(true);
+    expect(productDetails('mango', 'avocado')[1].details.productId === 'avocado123').toEqual(true);
+  });
+  // it('Verifica se a função `productDetails é uma função', () => {
+    // expect(typeof productDetails).toEqual(function);
+  // });
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
     // Teste se o retorno da função é um array.
@@ -36,5 +58,5 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se os dois itens dentro do array retornado pela função são objetos.
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
-  });
+  
 });
