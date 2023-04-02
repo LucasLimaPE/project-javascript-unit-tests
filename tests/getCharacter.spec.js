9cd29e7123243ddd94d3740c362f4395f1f8c9b9
LucasLimaPE <robertolucas011@gmail.com>
2023-04-02 19:07:34 -0300

    feat: implement test cases for the getCharacter function.

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -1,55 +1,48 @@
-/* eslint-disable max-len */
-/* eslint-disable no-unused-vars */
 
 const getCharacter = require('../src/getCharacter');
 
-/*
-Essa função recebe como parâmetro o nome de um personagem e retorna um objeto com seu nome, classe e frases.
-
-O retorno será de acordo com a seguinte relação:
-
- Parâmetro  |      Nome       |    Classe   |              Frases
-----------------------------------------------------------------------------------
-   Arya     |   Arya Stark    |    Rogue    | 'Not today', 'A girl has no name.'
-  Brienne   |  Brienne Tarth  |    Knight   | 'Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'
-Melissandre |   Melissandre   | Necromancer | 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'
-
-- Se o parâmetro não estiver na tabela, a função retorna undefined.
-- Se o parâmetro estiver, a função retorna um objeto no formato abaixo:
-
-{
-  name: 'Nome do Personagem',
-  class: 'Classe do Personagem' ,
-  phrases: ['frase1', 'frase2']
-}
-
-- OBS.: O parâmetro não é CASE SENSITIVE, portanto Arya, ArYa e ARYA tem o mesmo resultado.
-
-Elabore testes para verificar se a função está funcionando de acordo com o proposto.
-
-Parâmetros:
-  - Uma string.
-
-Comportamento: 
-  - getCharacter('Arya');
-
-Retorno:
-{
+const mockResultArya = {
   name: 'Arya Stark',
   class: 'Rogue',
-  phrases: [ 'Not today', 'A girl has no name.' ]
+  phrases: ['Not today', 'A girl has no name.']
 }
-*/
+
+const mockResultBrienne = {
+    name: 'Brienne Tarth',
+    class: 'Knight',
+    phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+}
+
+const mockResultMelissandre = {
+  name: 'Melissandre',
+  class: 'Necromancer',
+  phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+}
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se os parâmetros não são Case Sensitive.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+  it('Testa se a função, quando não recebe nenhum parâmetro, retorna undefined', () => {
+    expect(getCharacter()).toBeUndefined();
+  });
+
+  it('Testa se a função retorna o objeto correto para o parâmetro "Arya"', () => {
+    expect(getCharacter('Arya')).toStrictEqual(mockResultArya);
+  });
+
+  it('Testa se a função retorna o objeto correto para o parâmetro "Brienne"', () => {
+    expect(getCharacter('Brienne')).toStrictEqual(mockResultBrienne);
+  });
+
+  it('Testa se a função retorna o objeto correto para o parâmetro "Melissandre"', () => {
+    expect(getCharacter('Melissandre')).toStrictEqual(mockResultMelissandre);
+  });
+
+  it('Testa se os parâmetros não são Case Sensitive', () => {
+    expect(getCharacter('arya')).toStrictEqual(mockResultArya);
+    expect(getCharacter('MELISSANDRE')).toStrictEqual(mockResultMelissandre);
+    expect(getCharacter('bRiEnNe')).toStrictEqual(mockResultBrienne);
+  });
+
+  it('Testa se ao passar um nome que não está na tabela, a função retorna undefined.', () => {
+    expect(getCharacter('Ferdinand')).toBeUndefined();
   });
 });
