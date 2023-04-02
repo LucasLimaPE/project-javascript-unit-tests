688cd2053fea1758a970037ffdf583e56bcdf897
LucasLimaPE <robertolucas011@gmail.com>
2023-04-02 10:39:35 -0300

    feat: remove comentários que sujam o código e faz menção ao colega Guilherme Azevedo, adicionando um link para seu linkedin.

--- src/average.js
+++ src/average.js
@@ -1,18 +1,6 @@
-/*
-  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
-  Caso a função receba algum valor não númerico ou um array vazio,
-  o valor undefined deve ser retornado.
-  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
-
-  Parâmetros:
-    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
-  Comportamento:
-    - average([2, 2]) // Retorno: 2;
-    - average([1, 1]) // Retorno: 1;
-    - average([1, '2']) // Retorno: undefined;
-*/
-/*
+/* 
 Este código foi desenvolvido com ajuda do colega Guilherme Azevedo.
+https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAADe7FXIBKH8It-KnzlRxwicuRFH97Vi5md4&keywords=guilherme%20azevedo&origin=RICH_QUERY_TYPEAHEAD_HISTORY&position=0&searchId=acd2aed1-835c-49b2-9611-ca016cf0639f&sid=)QB
 */
 
 const average = (array) => {
